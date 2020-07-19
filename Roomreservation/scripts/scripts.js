3f245ffb5fc95763367454d8d8efddeb185dd834
wanjiku <elidakirigo@gmail.com>
2020-07-20 00:15:02 +0300

    new element design

---
+++ Roomreservation/scripts/scripts.js
@@ -0,0 +1,7 @@
+function updateHidden (choice) {
+hiddenField = document.getElementById('roomnumber');
+if(choice == "Room # 101") { hiddenField.value = "sehr geehrter"; 
+var msg="hello";
+alert(msg);}
+
+}
